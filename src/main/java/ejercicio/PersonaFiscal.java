fe23803b41c6134fcaea47d0145305527aa4c44a
Daniel <Daniel@DESKTOP-E94NK9S>
2018-03-15 16:33:24 +0100

    Primer commit y push

---
+++ src/main/java/ejercicio/PersonaFiscal.java
@@ -0,0 +1,37 @@
+package ejercicio;
+
+public class PersonaFiscal {
+
+	private String nombre;
+	private int edad;
+	private int sueldo;
+
+	public int calcularImpuesto() {
+
+		return 1;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String _nombre) {
+		this.nombre = _nombre;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(int _edad) {
+		this.edad = _edad;
+	}
+
+	public int getSueldo() {
+		return this.sueldo;
+	}
+
+	public void setSueldo(int _sueldo) {
+		this.sueldo = _sueldo;
+	}
+}
---
+++ src/main/java/ejercicio/TipoImpuesto.java
@@ -0,0 +1,33 @@
+package ejercicio;
+
+public class TipoImpuesto {
+
+	private int porcentaje;
+	private int porcentaje30;
+	private String nombre;
+
+	public int getPorcentaje() {
+		return this.porcentaje;
+	}
+
+	public void setPorcentaje(int _porcentaje) {
+		this.porcentaje = _porcentaje;
+	}
+
+	public int getPorcentaje30() {
+		return this.porcentaje30;
+	}
+
+	public void setPorcentaje30(int _porcentaje30) {
+		this.porcentaje30 = _porcentaje30;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String _nombre) {
+		this.nombre = _nombre;
+	}
+
+}
